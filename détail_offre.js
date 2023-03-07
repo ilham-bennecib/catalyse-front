adb5f2a34c19f61a804c8bacd68bf4626b213122
ilham-bennecib <ilham86@live.fr>
2023-03-08 00:33:12 +0100

    feat li in détail

--- détail_offre.js
+++ détail_offre.js
@@ -6,7 +6,7 @@ const offerId = urlParams.get('id');
 const divContainerOffer = document.querySelector('.container_offer');
 
 // Utiliser l'API Fetch pour récupérer les données JSON de l'offre d'emploi correspondante
-fetch(`http://localhost:3000/offer/${offerId}`)
+fetch(`http://localhost:5000/offer/${offerId}`)
   .then(response => response.json()) // Convertir la réponse en JSON
   .then(data => {
     // Afficher les détails de l'offre d'emploi dans la div correspondante
@@ -14,6 +14,42 @@ fetch(`http://localhost:3000/offer/${offerId}`)
     title.textContent = `Nom de l'offre : ${data.offer_name}`;
     title.classList.add('sub_title');
     divContainerOffer.appendChild(title);
+
+    const company = document.createElement('h4');
+    company.textContent = `Entreprise : ${data.company_name}`;
+    title.classList.add('h4');
+    divContainerOffer.appendChild(company);
+
+    const ulDetails = document.createElement('ul');
+    ulDetails.classList.add('ul_details_offer');
+
+    const liPubdate= document.createElement('li');
+    liPubdate.classList.add('li_detail');
+    liPubdate.textContent = `date de la publication de l'offre : ${data.publication_date}`;
+    ulDetails.appendChild(liPubdate);
+  
+
+    const liCandidacydate= document.createElement('li');
+    liCandidacydate.classList.add('li_detail');
+    liCandidacydate.textContent = `date de candidature : ${data.candidacy_date}`;
+    ulDetails.appendChild(liCandidacydate);
+
+    const liType= document.createElement('li');
+    liType.classList.add('li_detail');
+    liType.textContent = `Type de contrat : ${data.type}`;
+    ulDetails.appendChild(liType);
+
+    const liStatus= document.createElement('li');
+    liStatus.classList.add('li_detail');
+    liStatus.textContent = `Status: ${data.name}`;
+    ulDetails.appendChild(liStatus);
+
+    const liCandidacyStatus= document.createElement('li');
+    liCandidacyStatus.classList.add('li_detail');
+    liCandidacyStatus.textContent = `Status de la candidature: ${data.candidature}`;
+    ulDetails.appendChild(liCandidacyStatus);
+    
+    divContainerOffer.appendChild(ulDetails);
 
   
 
